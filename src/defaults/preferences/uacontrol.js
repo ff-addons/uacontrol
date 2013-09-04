ac6de802eb6544e126994c6643821a67382276a6
Ivan Kozik <ivan@ludios.org>
2013-09-04 10:52:54 +0000

    Import https://addons.mozilla.org/firefox/downloads/file/104262/uacontrol-0.1.1-fx.xpi

---
+++ src/defaults/preferences/uacontrol.js
@@ -0,0 +1,5 @@
+pref("extensions.uacontrol@qz.tsugumi.org.description", "chrome://uacontrol/locale/uacontrol.properties");
+pref("uacontrol.enabled", true);
+pref("uacontrol.actions", "");
+pref("uacontrol.statusbar", true);
+pref("uacontrol.contextMenu", true);
